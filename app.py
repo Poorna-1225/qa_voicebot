270571ce9cc6c0c14883cd58b180d801b34944e1
Poorna-1225 <nagasaikumarjampni1998@gmail.com>
2024-12-05 22:06:27 -0500

    comments added in app.py

--- app.py
+++ app.py
@@ -22,31 +22,37 @@ st.title("OpenAI Conversational Chatbot 🤖")
 # Create footer container for the microphone
 footer_container = st.container()
 with footer_container:
-    audio_bytes = audio_recorder()
+    audio_bytes = audio_recorder() # audio_bytes is having all the speech we record using recorder.
 
 
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
+    with st.chat_message(message["role"]): #st.chat_message creates a text field where we can write the text messages.
+        st.write(message["content"])  # st.write is used to write the text in the texfield we created. 
 
 if audio_bytes:
     # Write the audio bytes to a file
-    with st.spinner("Transcribing..."):
-        webm_file_path = "temp_audio.mp3"
+    with st.spinner("Transcribing..."): #st.spinner displays the spinner with given text next to it.
+        webm_file_path = "temp_audio.mp3" # webm_file_path = the string we defined.
         with open(webm_file_path, "wb") as f:
-            f.write(audio_bytes)
+            f.write(audio_bytes) # we are opening that file(temp_audio.mp3 which is stored in webm_file_path) and writing all the audio recorded.
+            #technically now temp_auido.mp3 stores audio recorded every single time.
 
-        transcript = speech_to_text(webm_file_path)
-        if transcript:
+        transcript = speech_to_text(webm_file_path) # we are calling stt function and passing the webm_file_path as parameter which is having the auido recorded in it and this function returns the text as ouptput. it converts the audio recorded into text.
+        if transcript: # if it is having text then we append that to the session_state.messages and then remove the file_path since it can store the next message.
             st.session_state.messages.append({"role": "user", "content": transcript})
             with st.chat_message("user"):
                 st.write(transcript)
             os.remove(webm_file_path)
 
 if st.session_state.messages[-1]["role"] != "assistant":
+    # it checks if the last message in the history is from the user.
+    # if it is from user, then it created a text field saying assistant and folled by spinner saying thinking
+    # then it calls the get_answer function from the utils.py file and pass the session_state.messages to get the answer.
+    # then generate the audio response by passing the text response as input.
+    # finally play the auido using the function auto_play 
     with st.chat_message("assistant"):
         with st.spinner("Thinking🤔..."):
-            final_response = get_answer(st.session_state.messages)
+            final_response = gets_answer(st.session_state.messages)
         with st.spinner("Generating audio response..."):    
             audio_file = text_to_speech(final_response)
             autoplay_audio(audio_file)
